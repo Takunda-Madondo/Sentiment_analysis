67d77f96c8f0a8996045b87b32c60a9e184584ec
Takunda Madondo <83583905+Takunda-Madondo@users.noreply.github.com>
2025-05-28 20:46:49 +0200

    Update app.py
    code description

--- app.py
+++ app.py
@@ -1,30 +1,43 @@
 import os
 from flask import Flask, request, render_template, jsonify
 from twitter import TwitterClient
-# app name
+
+# Initialize the Flask application
 app = Flask(__name__)
+
+# Create an instance of the TwitterClient with a specific username
 api = TwitterClient('@Ashish17969125')
 
+# Function to convert string to boolean
 def strtobool(v):
     return v.lower() in ["yes", "true", "t", "1"]
 
+# Route for the homepage
 @app.route('/')
 def index():
-
     return render_template('index.html')
 
+# Route to fetch tweets based on query parameters
 @app.route('/tweets')
 def tweets():
-        retweets_only = request.args.get('retweets_only')
-        api.set_retweet_checking(strtobool(retweets_only.lower()))
-        with_sentiment = request.args.get('with_sentiment')
-        api.set_with_sentiment(strtobool(with_sentiment.lower()))
-        query = request.args.get('query')
-        api.set_query(query)
+    # Get the retweets_only parameter from the request
+    retweets_only = request.args.get('retweets_only')
+    api.set_retweet_checking(strtobool(retweets_only.lower()))
+    
+    # Get the with_sentiment parameter from the request
+    with_sentiment = request.args.get('with_sentiment')
+    api.set_with_sentiment(strtobool(with_sentiment.lower()))
+    
+    # Get the query parameter from the request
+    query = request.args.get('query')
+    api.set_query(query)
 
-        tweets = api.get_tweets()
-        return jsonify({'data': tweets, 'count': len(tweets)})
+    # Fetch tweets based on the set parameters
+    tweets = api.get_tweets()
+    
+    # Return the tweets in JSON format
+    return jsonify({'data': tweets, 'count': len(tweets)})
 
+# Run the application
 if __name__ == '__main__':
     app.run()
-''
